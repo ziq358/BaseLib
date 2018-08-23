8d6f8520800d8753174b9f09fab08c6c384dc7a3
wuyanqiang <yqwut@isoftstone.com>
2018-08-23 21:44:38 +0800

    MediaCodec 解码 音频

--- app/src/main/java/com/ziq/baselib/Activity/MediacodecActivity.java
+++ app/src/main/java/com/ziq/baselib/Activity/MediacodecActivity.java
@@ -49,7 +49,7 @@ import butterknife.OnClick;
 public class MediacodecActivity extends BaseActivity {
 
     public static final String TAG = "Mediacodec";
-
+    private static final long TIMEOUT_US = 10000;
     @Bind(R.id.path)
     TextView pathTextView;
     @Bind(R.id.surface_view)
@@ -94,6 +94,7 @@ public class MediacodecActivity extends BaseActivity {
 
     private void play(){
         new VideoThread().start();
+        new AudioThread().start();
     }
 
     private class VideoThread extends Thread{
@@ -120,6 +121,7 @@ public class MediacodecActivity extends BaseActivity {
                 if (videoCodec != null) {
                     videoCodec.start();
                     MediaCodec.BufferInfo videoBufferInfo = new MediaCodec.BufferInfo();
+                    long startMs = System.currentTimeMillis();
                     while (!Thread.interrupted()) {
                         //不断读数据 写进 缓存
                         ByteBuffer[] inputBuffers = videoCodec.getInputBuffers();
@@ -151,7 +153,7 @@ public class MediacodecActivity extends BaseActivity {
                                 break;
                             default:
                                 //延时 -- 如果缓冲区里的可展示时间>当前视频播放的进度，就休眠一下
-//                                sleepRender(videoBufferInfo, startMs);
+                                sleepRender(videoBufferInfo, startMs);
                                 //渲染
                                 Log.e(TAG, "渲染 " + videoBufferInfo.presentationTimeUs);
                                 videoCodec.releaseOutputBuffer(outputBufferIndex, true);
@@ -172,8 +174,142 @@ public class MediacodecActivity extends BaseActivity {
         }
     }
 
+    private class AudioThread extends Thread{
+        private AudioTrack audioTrack;
+        private int audioInputBufferSize;
+
+        @Override
+        public void run() {
+            try {
+
+                MediaExtractor audioExtractor = new MediaExtractor();
+                audioExtractor.setDataSource(videoPath);
+                int audioTrackIndex = -1;
+                MediaFormat mediaFormat = null;
+                MediaCodec audioCodec = null;
+                for (int i = 0; i < audioExtractor.getTrackCount(); i++) {
+                    mediaFormat = audioExtractor.getTrackFormat(i);
+                    if (mediaFormat.getString(MediaFormat.KEY_MIME).startsWith("audio/")) {
+                        audioExtractor.selectTrack(i);
+                        audioTrackIndex = i;
+                        break;
+                    }
+                }
+                if(audioTrackIndex >= 0){
+
+                    int audioChannels = mediaFormat.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
+                    int audioSampleRate = mediaFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE);
+
+                    int minBufferSize = AudioTrack.getMinBufferSize(audioSampleRate,
+                            (audioChannels == 1 ? AudioFormat.CHANNEL_OUT_MONO : AudioFormat.CHANNEL_OUT_STEREO), AudioFormat.ENCODING_PCM_16BIT);
+
+                    int maxInputSize = mediaFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE);
+                    audioInputBufferSize = minBufferSize > 0 ? minBufferSize * 4 : maxInputSize;
+                    int frameSizeInBytes = audioChannels * 2;
+                    audioInputBufferSize = (audioInputBufferSize / frameSizeInBytes) * frameSizeInBytes;
+
+                    audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, audioSampleRate,
+                            (audioChannels == 1 ? AudioFormat.CHANNEL_OUT_MONO : AudioFormat.CHANNEL_OUT_STEREO), AudioFormat.ENCODING_PCM_16BIT, audioInputBufferSize, AudioTrack.MODE_STREAM);
+                    audioTrack.play();
+
+                    audioCodec = MediaCodec.createDecoderByType(mediaFormat.getString(MediaFormat.KEY_MIME));
+                    audioCodec.configure(mediaFormat, null, null, 0);
+
+                }
+
+                if (audioCodec == null) {
+                    Log.v(TAG, "audio decoder null");
+                    return;
+                }
+
+                audioCodec.start();
+
+                final ByteBuffer[] buffers = audioCodec.getOutputBuffers();
+                int sz = buffers[0].capacity();
+                if (sz <= 0)
+                    sz = audioInputBufferSize;
+                byte[] mAudioOutTempBuf = new byte[sz];
+
+                MediaCodec.BufferInfo audioBufferInfo = new MediaCodec.BufferInfo();
+                ByteBuffer[] inputBuffers = audioCodec.getInputBuffers();
+                ByteBuffer[] outputBuffers = audioCodec.getOutputBuffers();
 
 
+                while (!Thread.interrupted()) {
+                    //不断读数据 写进 缓存
+                    int inputBufferIndex = audioCodec.dequeueInputBuffer(10000);
+                    if (inputBufferIndex >= 0) {
+                        ByteBuffer inputBuffer = inputBuffers[inputBufferIndex];
+                        int sampleSize = audioExtractor.readSampleData(inputBuffer, 0);
+                        if (sampleSize < 0) {
+                            //告诉 解码器 到达 文件 末尾
+                            audioCodec.queueInputBuffer(inputBufferIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+                            Log.e(TAG, "到文件末尾");
+                        } else {
+                            Log.e(TAG, "写数据进缓存 "+sampleSize);
+                            audioCodec.queueInputBuffer(inputBufferIndex, 0, sampleSize, audioExtractor.getSampleTime(), 0);
+                            audioExtractor.advance();
+                        }
+                    }
+
+                    //取 缓存
+                    int outputBufferIndex = audioCodec.dequeueOutputBuffer(audioBufferInfo, 10000);
+                    switch (outputBufferIndex) {
+                        case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:
+                            Log.e(TAG, "取 缓存 format changed");
+                            break;
+                        case MediaCodec.INFO_TRY_AGAIN_LATER:
+                            Log.e(TAG, "取 缓存 超时");
+                            break;
+                        case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED:
+                            outputBuffers = audioCodec.getOutputBuffers();
+                            Log.e(TAG, "取 缓存 output buffers changed");
+                            break;
+                        default:
+
+                            ByteBuffer outputBuffer = outputBuffers[outputBufferIndex];
+                            if (audioBufferInfo.size > 0) {
+                                if (mAudioOutTempBuf.length < audioBufferInfo.size) {
+                                    mAudioOutTempBuf = new byte[audioBufferInfo.size];
+                                }
+                                outputBuffer.position(0);
+                                outputBuffer.get(mAudioOutTempBuf, 0, audioBufferInfo.size);
+                                outputBuffer.clear();
+                                if (audioTrack != null)
+                                    audioTrack.write(mAudioOutTempBuf, 0, audioBufferInfo.size);
+                            }
+                            audioCodec.releaseOutputBuffer(outputBufferIndex, false);
+                            break;
+                    }
+
+                    if ((audioBufferInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+                        Log.e(TAG, "取 缓存 buffer stream end  退出循环");
+                        break;
+                    }
+                }
+                audioCodec.stop();
+                audioCodec.release();
+                audioExtractor.release();
+                audioTrack.stop();
+                audioTrack.release();
+
+            } catch (Exception e) {
+                Log.e(TAG, "VideoThread --- "+e.getMessage());
+            }
+        }
+    }
+
+    //延迟渲染
+    private void sleepRender(MediaCodec.BufferInfo audioBufferInfo, long startMs) {
+        while (audioBufferInfo.presentationTimeUs / 1000 > System.currentTimeMillis() - startMs) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
 
 
     /*
