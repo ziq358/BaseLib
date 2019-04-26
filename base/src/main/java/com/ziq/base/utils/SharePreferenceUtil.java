1580b684bc77f4dd927575c7246231668dd089d9
wuyanqiang <461804160@qq.com>
2019-04-26 16:57:39 +0800

    3.1.9 版本 SharePreferenceUtil

---
+++ base/src/main/java/com/ziq/base/utils/SharePreferenceUtil.java
@@ -0,0 +1,171 @@
+package com.ziq.base.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+
+public class SharePreferenceUtil {
+
+    private SharePreferenceUtil() {
+    }
+
+    /**
+     * 保存在手机里面的文件名
+     */
+    public static final String FILE_NAME = "share_data";
+
+    /**
+     * 保存数据的方法，我们需要拿到保存数据的具体类型，然后根据类型调用不同的保存方法
+     *
+     * @param context 上下文
+     * @param key     标识
+     * @param value   参数
+     */
+    public static void save(Context context, String key, Object value) {
+        if (value == null) {
+            return;
+        }
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        if (value instanceof String) {
+            editor.putString(key, (String) value);
+        } else if (value instanceof Integer) {
+            editor.putInt(key, (Integer) value);
+        } else if (value instanceof Boolean) {
+            editor.putBoolean(key, (Boolean) value);
+        } else if (value instanceof Float) {
+            editor.putFloat(key, (Float) value);
+        } else if (value instanceof Long) {
+            editor.putLong(key, (Long) value);
+        } else {
+            throw new IllegalArgumentException("数据类型不支持");
+        }
+
+        SharedPreferencesCompat.apply(editor);
+    }
+
+    /**
+     * 得到保存数据的方法，我们根据默认值得到保存的数据的具体类型，然后调用相对于的方法获取值
+     *
+     * @param context      上下文
+     * @param key          标识
+     * @param defaultValue 根据该参数进行判断读取的是什么类型的SharedPreferences值，而且这个也是默认值， 不能设置为null
+     * @return
+     */
+    public static Object get(Context context, String key, Object defaultValue) {
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+
+        if (defaultValue instanceof String) {
+            return sp.getString(key, (String) defaultValue);
+        } else if (defaultValue instanceof Integer) {
+            return sp.getInt(key, (Integer) defaultValue);
+        } else if (defaultValue instanceof Boolean) {
+            return sp.getBoolean(key, (Boolean) defaultValue);
+        } else if (defaultValue instanceof Float) {
+            return sp.getFloat(key, (Float) defaultValue);
+        } else if (defaultValue instanceof Long) {
+            return sp.getLong(key, (Long) defaultValue);
+        }
+
+        return null;
+    }
+
+    /**
+     * 移除某个key值已经对应的值
+     *
+     * @param context 上下文
+     * @param key     标识
+     */
+    public static void remove(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME,
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.remove(key);
+        SharedPreferencesCompat.apply(editor);
+    }
+
+    /**
+     * 清除所有数据
+     *
+     * @param context 上下文
+     */
+    public static void clear(Context context) {
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.clear();
+        SharedPreferencesCompat.apply(editor);
+    }
+
+    /**
+     * 查询某个key是否已经存在
+     *
+     * @param context 上下文
+     * @param key     标识
+     * @return 如果存在返回true， 否则false
+     */
+    public static boolean contains(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        return sp.contains(key);
+    }
+
+    /**
+     * 返回所有的键值对
+     *
+     * @param context 上下文
+     * @return
+     */
+    public static Map<String, ?> getAll(Context context) {
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
+        return sp.getAll();
+    }
+
+    /**
+     * 创建一个解决SharedPreferencesCompat.apply方法的一个兼容类
+     * <p/>
+     * Created by hugo on 15/7/31.
+     */
+    private static class SharedPreferencesCompat {
+        private static final Method sApplyMethod = findApplyMethod();
+
+        /**
+         * 反射查找apply的方法
+         * @return
+         */
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        private static Method findApplyMethod() {
+            try {
+                Class clz = SharedPreferences.Editor.class;
+                return clz.getMethod("apply");
+            } catch (NoSuchMethodException e) {
+            }
+
+            return null;
+        }
+
+        /**
+         * 如果找到则使用apply执行，否则使用commit
+         *
+         * @param editor
+         */
+        public static void apply(SharedPreferences.Editor editor) {
+            try {
+                if (sApplyMethod != null) {
+                    sApplyMethod.invoke(editor);
+                    return;
+                }
+            } catch (IllegalArgumentException e) {
+            } catch (IllegalAccessException e) {
+            } catch (InvocationTargetException e) {
+            }
+            editor.commit();
+        }
+    }
+
+
+
+}
