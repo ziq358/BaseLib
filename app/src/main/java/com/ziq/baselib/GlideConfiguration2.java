c8a68fe66a41759d5cd155fd9639b5f7b87ca5dd
wuyanqiang <461804160@qq.com>
2019-06-06 18:02:07 +0800

    Glide 配置 测试

---
+++ app/src/main/java/com/ziq/baselib/GlideConfiguration2.java
@@ -0,0 +1,8 @@
+package com.ziq.baselib;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+//@GlideModule
+public class GlideConfiguration2 extends AppGlideModule {
+}
---
+++ base/src/main/java/com/ziq/base/glide/GlideConfiguration.java
@@ -0,0 +1,8 @@
+package com.ziq.base.glide;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class GlideConfiguration extends AppGlideModule {
+}
