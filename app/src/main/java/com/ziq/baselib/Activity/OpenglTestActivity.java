6706a90a82eb0a3eb005e0aa97cc538693da179b
wuyanqiang <461804160@qq.com>
2019-06-24 12:24:24 +0800

    opengl 360图片 + 陀螺仪

--- app/src/main/java/com/ziq/baselib/Activity/OpenglTestActivity.java
+++ app/src/main/java/com/ziq/baselib/Activity/OpenglTestActivity.java
@@ -1,6 +1,11 @@
 package com.ziq.baselib.Activity;
 
+import android.content.Context;
 import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Handler;
@@ -31,7 +36,7 @@ import javax.microedition.khronos.opengles.GL10;
 import butterknife.BindView;
 import butterknife.OnClick;
 
-public class OpenglTestActivity extends MvpBaseActivity{
+public class OpenglTestActivity extends MvpBaseActivity implements SensorEventListener {
 
     @BindView(R.id.glSurfaceView)
     GLSurfaceView glSurfaceView;
@@ -40,6 +45,13 @@ public class OpenglTestActivity extends MvpBaseActivity{
     GestureDetector gestureDetector;
     private static final float sDensity =  Resources.getSystem().getDisplayMetrics().density;
     private static final float sDamping = 0.2f;//阻尼
+
+    SensorManager sensorManager;
+    Sensor gyroscopeSensor;
+
+    private final float NS2S = 1.0f / 1000000000.0f;
+    long timestamp;
+
     @Override
     public int initLayoutResourceId() {
         return R.layout.activity_opengl_test;
@@ -60,6 +72,7 @@ public class OpenglTestActivity extends MvpBaseActivity{
         gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener(){
             @Override
             public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                Log.e("ziq", "度数1: "+distanceX / sDensity * sDamping + " "+ distanceY / sDensity * sDamping);
                 myRenderer.mDeltaX = (myRenderer.mDeltaX + distanceX / sDensity * sDamping);
                 myRenderer.mDeltaY = (myRenderer.mDeltaY + distanceY / sDensity * sDamping);
                 return super.onScroll(e1, e2, distanceX, distanceY);
@@ -73,6 +86,46 @@ public class OpenglTestActivity extends MvpBaseActivity{
                 return gestureDetector.onTouchEvent(motionEvent);
             }
         });
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);// 陀螺仪传感器，单位是rad/s，测量设备x、y、z三轴的角加速度
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //SensorManager.SENSOR_DELAY_GAME 影响回调次数， 敏感度够了才不会卡顿
+        sensorManager.registerListener(this, gyroscopeSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            if (timestamp != 0) {
+                final float dT = (event.timestamp - timestamp) * NS2S;
+                float deltaX = (float) Math.toDegrees(event.values[0] * dT);
+                float deltaY = (float) Math.toDegrees(event.values[1] * dT);
+                float deltaZ = (float) Math.toDegrees(event.values[2] * dT);
+                Log.e("ziq", "度数2: "+deltaX + " "+ deltaY+ " "+ deltaZ );
+                //方向相反 减法
+                myRenderer.mDeltaX = (myRenderer.mDeltaX - deltaY);
+                myRenderer.mDeltaY = (myRenderer.mDeltaY - deltaX);
+
+            }
+            timestamp = event.timestamp;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 
     @Override
@@ -80,4 +133,6 @@ public class OpenglTestActivity extends MvpBaseActivity{
         myRenderer.onDestry();
         super.onDestroy();
     }
+
+
 }
